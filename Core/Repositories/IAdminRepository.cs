a0f10808bd29e581a8db8b52560e3cb431d5d654
mertgogersin <83244162+mertgogersin@users.noreply.github.com>
2021-09-20 23:24:20 +0300

    Update IAdminRepository.cs

--- Core/Repositories/IAdminRepository.cs
+++ Core/Repositories/IAdminRepository.cs
@@ -9,7 +9,8 @@ namespace Core.Repositories
         Task SetSubscriptionPlanForUser(string id, SubscriptionPlans type);
         Task UpdateCompanyInfo(Company company);      
         Task SetPassiveCompany(string company);
-
+        Task SetVacationType(Vacation vacation); //revize yapılacak
+        Task ActivateUser(string userID);
 
     }
 }
