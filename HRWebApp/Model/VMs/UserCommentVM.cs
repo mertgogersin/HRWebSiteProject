8555f37b856a535cfe2b66dde944af3181486171
mervegulcan <gulcan.mrv@gmail.com>
2021-10-14 09:52:56 +0300

    Added images to index page

---
+++ HRWebApp/Model/VMs/UserCommentVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRWebApp.Model.VMs
+{
+    public class UserCommentVM
+    {
+        public byte[] Logo { get; set; }
+    }
+}
