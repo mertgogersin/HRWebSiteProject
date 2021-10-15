27e4a6de221b76ca56e80fd51f68633d3bd7ffda
mertgogersin <mert.gogersin1@gmail.com>
2021-10-15 15:05:06 +0300

    Register and Login is done

--- HRWebApp/Controllers/LoginController.cs
+++ HRWebApp/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,12 @@ namespace HRWebApp.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet("{id}")]
+        public IActionResult LoginUser(string id)
+        {            
+            HttpContext.Session.SetString("userID", id);
+            return Ok("Success");
         }
     }
 }
