fa48cf3f6871e20481ddf6e2f6e729bb167258c5
mertgogersin <83244162+mertgogersin@users.noreply.github.com>
2021-10-23 20:29:37 +0300

    Update Admin.cs

--- Core/Entities/Admin.cs
+++ Core/Entities/Admin.cs
@@ -11,7 +11,6 @@ namespace Core.Entities
         public int AdminID { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        //public string GithubDeneme { get; set; }
-        //public string GithubDeneme2 { get; set; }
+       
     }
 }
