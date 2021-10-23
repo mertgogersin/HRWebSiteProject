938703d9670f44285e8940201dd21cc356c9c9b9
mertgogersin <mert.gogersin1@gmail.com>
2021-10-23 20:28:30 +0300

    deneme

--- Core/Entities/Admin.cs
+++ Core/Entities/Admin.cs
@@ -11,7 +11,7 @@ namespace Core.Entities
         public int AdminID { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        //public string GithubDeneme { get; set; }
-        //public string UygulamaDeneme { get; set; }
+        
+
     }
 }
